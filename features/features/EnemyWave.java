c60a3e58123e795371edf689deb6a78c650b771f
Anton Ekström <anton.e.ekstrom@gmail.com>
2022-12-01 11:37:23 +0100

    Replaces project (again)

---
+++ features/features/EnemyWave.java
@@ -0,0 +1,12 @@
+package features;
+
+import java.awt.geom.Point2D;
+
+public class EnemyWave {
+    public Point2D.Double fireLocation;
+    public long fireTime;
+    public double bulletVelocity, directAngle, distanceTraveled;
+    public int direction;
+
+    public EnemyWave() { }
+}
